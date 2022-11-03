9e4861d2c3685f21e957c0842aa37b45595a5783
Julia Papiernik <312518@via.dk>
2022-11-03 08:48:32 +0100

    #2

---
+++ FileData/DAOs/UserFileDao.cs
@@ -0,0 +1,39 @@
+﻿using Application.DaoInterfaces;
+using Domain.Models;
+
+namespace FileData.DAOs;
+
+public class UserFileDao : IUserDao
+{
+    private readonly FileContext _context;
+
+    public UserFileDao(FileContext context)
+    {
+        _context = context;
+    }
+
+    public Task<User> CreateAsync(User user)
+    {
+        int userId = 1;
+        if (_context.Users.Any())
+        {
+            userId = _context.Users.Max(u => u.Id);
+            userId++;
+        }
+
+        user.Id = userId;
+        
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        return Task.FromResult(user);
+    }
+
+    public Task<User?> GetByUsernameAsync(string userName)
+    {
+        User? existing = _context.Users.FirstOrDefault(
+            u => u.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase)
+        );
+        return Task.FromResult(existing);
+    }
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class UserController : ControllerBase
+{
+    private readonly IUserLogic _userLogic;
+
+    public UserController(IUserLogic userLogic)
+    {
+        _userLogic = userLogic;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateAsync(UserCreationDto dto)
+    {
+        try
+        {
+            User user = await _userLogic.CreateAsync(dto);
+            return Created($"/users/{user.Id}", user);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
